2dfafa09ce327aad9375fcebc93fe4d0668fcd63
Alejandro Dorado <a.dorado160@uniandes.edu.co>
2019-11-04 16:23:09 -0500

    Dirty getallstacks

--- app/src/main/java/com/example/tap/ShowStackTrace.java
+++ app/src/main/java/com/example/tap/ShowStackTrace.java
@@ -4,8 +4,6 @@ import android.os.AsyncTask;
 import android.view.View;
 import android.widget.TextView;
 
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.Map;
 
 public class ShowStackTrace extends AsyncTask<Void,Void,String> {
